94b72c3be5825be8289209f2b6d234ca32bb9ca2
Yevhen Malyshko <memx86@gmail.com>
2022-01-04 12:40:52 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
-
+const formRef = document.querySelector(".login-form");
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Все поля должны быть заполнены");
+  } else {
+    const formData = {};
+    for (const { name, value } of event.currentTarget.elements) {
+      if (name) {
+        formData[name] = value;
+      }
+    }
+    console.log(formData);
+    event.currentTarget.reset();
+  }
+};
+formRef.addEventListener("submit", onFormSubmit);
