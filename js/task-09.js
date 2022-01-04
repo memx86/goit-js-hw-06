495dd3014bcd50fde472f60ade6cda7d7a652484
Yevhen Malyshko <memx86@gmail.com>
2022-01-04 12:54:29 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  button: document.querySelector(".change-color"),
+  spanColor: document.querySelector(".color"),
+  body: document.body,
+};
+const onChangeColorClick = () => {
+  const newBgColor = getRandomHexColor();
+  refs.spanColor.textContent = newBgColor;
+  refs.body.style.backgroundColor = newBgColor;
+};
+refs.button.addEventListener("click", onChangeColorClick);
