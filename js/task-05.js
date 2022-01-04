5e287525b91e85a7c29c00dc172c8726f29eaa0b
Yevhen Malyshko <memx86@gmail.com>
2022-01-04 11:33:33 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const refs = {
+  input: document.querySelector("#name-input"),
+  output: document.querySelector("#name-output"),
+};
+const onInputChange = (event) => {
+  console.log(event.currentTarget.value);
+  if (event.currentTarget.value) {
+    refs.output.textContent = event.currentTarget.value;
+  } else refs.output.textContent = "Anonymous";
+};
+refs.input.addEventListener("input", onInputChange);
